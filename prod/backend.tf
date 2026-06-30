d18318153e11deabef1d677060bfcc131f47bd23
tanyaa09 <iamtanya940@gmail.com>
2026-06-30 15:21:10 +0530

    cganges

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
+    resource_group_name  = "jenkins-rg"
     storage_account_name = "tanyastorage11111"
     container_name       = "prod"
     key                  = "terraform.tfstate"
